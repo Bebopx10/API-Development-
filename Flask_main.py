6b2bcdb47649d34757aa131c0f9105ce491d2f9d
Moe Garcia <guillermogarcia@lewisu.edu>
2026-04-30 12:49:21 -0500

    Fixed API connection issue

--- Flask_main.py
+++ Flask_main.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from databaseActions import DatabaseActions
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
